1a58b1ace2731dfefaf0188e81e4fd030f50011e
konstantinosGombakis <konstantinos.gombakis@eurodyn.com>
2024-02-29 13:06:46 +0200

    scripts update

--- commands_reverse_proxy_URL/getTokenForOrion.sh
+++ commands_reverse_proxy_URL/getTokenForOrion.sh
@@ -6,20 +6,14 @@ export $(cat ../.env | grep "#" -v)
 
 # token=$(curl --insecure --location --request POST 'http://localhost:8080/realms/fiware-server/protocol/openid-connect/token' \
 
-if [ "$HOST" == "localhost" ]; then
-  INSECURE=' --insecure '
-else
-  INSECURE=''
-fi
-
-token=$(curl -s $INSECURE --location --request POST 'https://'"${HOST}"'/idm/realms/fiware-server/protocol/openid-connect/token' \
+token=$(curl -s --insecure --location --request POST 'https://'"${HOST}"'/idm/realms/fiware-server/protocol/openid-connect/token' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --data-urlencode 'username=admin-user' \
 --data-urlencode 'password=admin-user' \
 --data-urlencode 'grant_type=password' \
---data-urlencode 'client_id=mintaka-pep' \
---data-urlencode 'client_secret=zQXGjcxPWuF7YBpAiFtaIOEIOrCiWqeH' | jq .access_token )
+--data-urlencode 'client_id=orion-pep' \
+--data-urlencode 'client_secret=yWv2aRCm3KKMGrj9lMXQcEXY4v80tcFk' | jq .access_token )
 # remove starting and tailing double quatas "
 # echo $token
 token=$(sed -e 's/^"//' -e 's/"$//' <<<"$token")
-echo -n "$token" > tokenMintaka.txt
+echo -n "$token" > token.txt
