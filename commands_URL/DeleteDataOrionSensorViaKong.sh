9ce6510faac4a4f2baf04ef793550a67bb33ede5
konstantinosGombakis <konstantinos.gombakis@eurodyn.com>
2024-10-03 11:43:23 +0300

    add command to delete entity

---
+++ commands_URL/DeleteDataOrionSensorViaKong.sh
@@ -0,0 +1,32 @@
+# #!/bin/bash
+# #
+# #  curl commands to reload the data from the previous tutorial
+# #
+
+# set -e
+# export $(cat ../.env | grep "#" -v)
+# export $(cat ./partner_variables.txt | grep "#" -v)
+
+# ./getTokenForOrion.sh
+# token=$(cat "token.txt")
+
+
+# if [ $# -eq 0 ]; then
+#   echo "No sensor as input "
+#   exit 0
+# elif [ $# -eq 1 ]; then
+#     entity="$1"
+#     echo $entity
+# fi
+
+
+
+# KONG_URL='https://'"${HOST}"'/kong/keycloak-orion'
+# # echo $KONG_URL
+# curl -s -G -X DELETE ''"${KONG_URL}"'/ngsi-ld/v1/entities/'"${entity}"'' \
+#   -H 'NGSILD-Tenant: circuloos_ed_leather' \
+#   -H 'NGSILD-Path: /' \
+#   -H 'Link: <'"${CONTEXT}"'>; rel="http://www.w3.org/ns/json-ld#context"; type="application/ld+json"' \
+#   -H 'Accept: application/ld+json' \
+#   -H 'Authorization: Bearer '"${token}"' ' | jq
+
