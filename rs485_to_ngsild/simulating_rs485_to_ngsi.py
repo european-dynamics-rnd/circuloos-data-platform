e7747c84f425503d6fec1d3ca016bb3414d02241
konstantinosGombakis <143802653+konstantinosGombakis@users.noreply.github.com>
2025-01-08 14:41:50 +0200

    ngsild_agent as class and simulating for testing

---
+++ rs485_to_ngsild/simulating_rs485_to_ngsi.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import os
+import sys
+import logging
+import decode_serial_data
+
+from ngsild_agent import NGSILDAgent
+
+from dataclasses import dataclass, asdict
+from typing import Optional
+
+from ngsildclient import Entity
+
+
+@dataclass
+class ExtendedType1SerialData(decode_serial_data.type1_serial_data):
+    id: str 
+    type: str  
+    observedAt: Optional[str] =None
+    
+
+logging.basicConfig(level=logging.DEBUG)
+_logger = logging.getLogger(__name__)
+
+agent = NGSILDAgent(_logger)
+
+serial_data_incoming =  "#1;342.60;43;51;37;66;44;23;154;59;7#"
+
+type1_data=decode_serial_data.parse_input_type1(serial_data_incoming)
+print(type1_data)
+
+type1_data_extended = ExtendedType1SerialData(
+    **asdict(type1_data),  # Unpack original data
+    id="urn:ngsi-ld:circuloos:serial:id1",
+    type="type1_serial_data",
+    observedAt="2025-01-01T09:19:03Z"
+)
+
+print(type1_data_extended)
+
+ngsi_ld=agent.generate_ngsild_entity(asdict(type1_data_extended))
+
+try:
+    responses=agent.get_cb_info_with_token()
+except Exception as e:
+    print(f"An error occurred: {e}")
+   
+if ngsi_ld is None:
+   print("no data")
+
+else:
+    try:
+        responses=agent.post_ngsi_to_cb_with_token(ngsi_ld)
+    except Exception as e:
+        _logger.error(f"An error occurred: {e}")
+
+
+            
+
+
+     
+     
+     
+     
