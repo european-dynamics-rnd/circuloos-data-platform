19fa781fe249f9e69e2ec4e79da43d82ae64dca3
konstantinosGombakis <konstantinos.gombakis@eurodyn.com>
2025-11-07 15:27:49 +0200

    remove default values

--- commands/addDataOrion.sh
+++ commands/addDataOrion.sh
@@ -3,10 +3,20 @@
 set -e
 export $(cat ../.env | grep "#" -v)
 
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <json_file> <tenant>"
+    echo "  json_file: Path to the JSON file to upload"
+    echo "  tenant: NGSILD-Tenant value"
+    exit 1
+fi
+
+jsonFile="$1"
+TENANT="$2"
+
 
 curl -iL -X POST  'http://'"${HOST}"':'"${ORION_LD_PORT}"'/ngsi-ld/v1/entityOperations/upsert' \
--H 'NGSILD-Tenant: circuloos_demo' \
+-H 'NGSILD-Tenant: '"${TENANT}"'' \
 -H 'NGSILD-Path: /' \
 -H 'Content-Type: application/ld+json' \
--d @$1
+-d @$jsonFile
 echo -e
--- commands/appendAttributesOrion.sh
+++ commands/appendAttributesOrion.sh
@@ -3,22 +3,22 @@
 set -e
 export $(cat ../.env | grep "#" -v)
 
-if [ $# -lt 2 ]; then
-  echo "Usage: $0 <entity_id> <json_file>"
-  echo "Example: $0 urn:ngsi-ld:Building:001 attributes.json"
+if [ $# -lt 3 ]; then
+  echo "Usage: $0 <TENANT> <entity_id> <json_file>"
+  echo "Example: $0 circuloos_demo urn:ngsi-ld:Building:001 attributes.json"
   exit 1
 fi
 
-entity="$1"
-json_file="$2"
+TENANT="$1"
+entity="$2"
+json_file="$3"
 echo "Appending attributes to entity: $entity"
 echo "From file: $json_file"
 
 
 curl -s -iX POST 'http://'"${HOST}"':'"${ORION_LD_PORT}"'/ngsi-ld/v1/entities/'"${entity}"'/attrs' \
 -H 'Content-Type: application/json' \
--H 'NGSILD-Tenant: circuloos_demo' \
--H 'NGSILD-Path: /' \
+-H 'NGSILD-Tenant: '"${TENANT}"'' \
 -H 'Link: <'"${CONTEXT}"'>; rel="http://www.w3.org/ns/json-ld#context"; type="application/ld+json"' \
 -d @"${json_file}"
 echo -e
