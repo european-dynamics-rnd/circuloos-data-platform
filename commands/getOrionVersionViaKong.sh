5db2a265c686dc6f707b96e9f86bde84fa17c4d6
konstantinosGombakis <konstantinos.gombakis@eurodyn.com>
2026-03-04 14:44:22 +0200

    update local file

--- commands/getOrionVersionViaKong.sh
+++ commands/getOrionVersionViaKong.sh
@@ -3,6 +3,12 @@ set -e
 export $(cat ../.env | grep "#" -v)
 
 
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <tenant>"
+    echo "  tenant: NGSILD-Tenant value"
+    exit 1
+fi
+
 if [ "$HOST" == "localhost" ]; then
   INSECURE=' --insecure '
 else
@@ -17,4 +23,5 @@ token=$(cat "token.txt")
 KONG_URL='https://'"${HOST}"':'"${KONG_PORT}"'/keycloak-orion'
 # echo $KONG_URL
 curl -s $INSECURE ''"${KONG_URL}"'/version' \
+ -H 'NGSILD-Tenant: '"${TENANT}"'' \
  --header 'Authorization: Bearer '"${token}"' ' | jq
